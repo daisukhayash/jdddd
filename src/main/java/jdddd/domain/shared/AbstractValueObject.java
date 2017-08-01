8dcf10b3d6831866df1573631c018c757b64c39c
daisukhayash <daisukhayash@gmail.com>
2017-08-01 23:34:26 +0900

    [fix] Fix indent

--- src/main/java/jdddd/domain/shared/AbstractValueObject.java
+++ src/main/java/jdddd/domain/shared/AbstractValueObject.java
@@ -5,27 +5,27 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 public abstract class AbstractValueObject implements ValueObject {
-    @Override
-    public boolean equals(final Object other) {
-        if (other == null) {
-            return false;
-        }
-        if (other == this) {
-            return true;
-        }
-        if (other.getClass() != this.getClass()) {
-            return false;
-        }
-        return EqualsBuilder.reflectionEquals(this, other);
-    }
-    
-    @Override
-    public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this);
-    }
-    
-    @Override
-    public String toString() {
-    	return ReflectionToStringBuilder.toString(this);
-    }
+	@Override
+	public boolean equals(final Object other) {
+		if (other == null) {
+			return false;
+		}
+		if (other == this) {
+			return true;
+		}
+		if (other.getClass() != this.getClass()) {
+			return false;
+		}
+		return EqualsBuilder.reflectionEquals(this, other);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		return ReflectionToStringBuilder.toString(this);
+	}
 }

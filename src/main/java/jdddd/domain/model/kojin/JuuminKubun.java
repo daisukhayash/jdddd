1d9a65025d7b3851ed06445dab20e6af501bf90d
daisukhayash <daisukhayash@gmail.com>
2017-08-03 23:03:05 +0900

    [fix] typo

--- src/main/java/jdddd/domain/model/kojin/JuuminKubun.java
+++ src/main/java/jdddd/domain/model/kojin/JuuminKubun.java
@@ -2,14 +2,14 @@ package jdddd.domain.model.kojin;
 
 import jdddd.domain.shared.Code;
 
-public enum JyuuminKubun implements Code {
+public enum JuuminKubun implements Code {
 	日本人("0"), //
 	外国人("1"), //
 	;
 
 	private String code;
 
-	private JyuuminKubun(String code) {
+	private JuuminKubun(String code) {
 		this.code = code;
 	}
 
--- src/test/java/jdddd/domain/model/kojin/JuuminKubunTest.java
+++ src/test/java/jdddd/domain/model/kojin/JuuminKubunTest.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class JyuuminKubunTest {
+public class JuuminKubunTest {
 
 	@Test
 	public void testCode() {
-		assertEquals("0", JyuuminKubun.日本人.code());
-		assertEquals("1", JyuuminKubun.外国人.code());
+		assertEquals("0", JuuminKubun.日本人.code());
+		assertEquals("1", JuuminKubun.外国人.code());
 	}
 
 }

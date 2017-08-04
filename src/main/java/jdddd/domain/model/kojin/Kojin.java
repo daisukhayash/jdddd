f0987d5c60608cf2ac0f699b901a4b7770c83799
daisukhayash <daisukhayash@gmail.com>
2017-08-04 22:44:56 +0900

    [update] Add JuuminKubun and Simei to Kojin

--- src/main/java/jdddd/domain/model/kojin/Kojin.java
+++ src/main/java/jdddd/domain/model/kojin/Kojin.java
@@ -12,12 +12,16 @@ public class Kojin extends AbstractEntity<Kojin> {
 	private AtenaBangou 宛名番号;
 	private KaiseiSeq 改製ＳＥＱ;
 	private SetaiBangou 世帯番号;
+	private JuuminKubun 住民区分;
+	private Simei 氏名;
 
 	public static class Builder {
 		// 必須
 		private final AtenaBangou 宛名番号;
 		private final KaiseiSeq 改製ＳＥＱ;
 		private SetaiBangou 世帯番号;
+		private JuuminKubun 住民区分;
+		private Simei 氏名;
 
 		public Builder(AtenaBangou 宛名番号, KaiseiSeq 改製ＳＥＱ) {
 			this.宛名番号 = 宛名番号;
@@ -26,6 +30,16 @@ public class Kojin extends AbstractEntity<Kojin> {
 
 		public Builder 世帯番号(SetaiBangou 世帯番号) {
 			this.世帯番号 = 世帯番号;
+			return this;
+		}
+
+		public Builder 住民区分(JuuminKubun 住民区分) {
+			this.住民区分 = 住民区分;
+			return this;
+		}
+
+		public Builder 氏名(Simei 氏名) {
+			this.氏名 = 氏名;
 			return this;
 		}
 
@@ -40,6 +54,9 @@ public class Kojin extends AbstractEntity<Kojin> {
 		this.set宛名番号(builder.宛名番号);
 		this.set改製ＳＥＱ(builder.改製ＳＥＱ);
 		this.set世帯番号(builder.世帯番号);
+		this.set住民区分(builder.住民区分);
+		this.set氏名(builder.氏名);
+
 	}
 
 	@Override
@@ -72,4 +89,21 @@ public class Kojin extends AbstractEntity<Kojin> {
 	public SetaiBangou 世帯番号() {
 		return this.世帯番号;
 	}
+
+	public void set住民区分(JuuminKubun 住民区分) {
+		this.住民区分 = 住民区分;
+	}
+
+	public JuuminKubun 住民区分() {
+		return this.住民区分;
+	}
+
+	public void set氏名(Simei 氏名) {
+		this.氏名 = 氏名;
+	}
+
+	public Simei 氏名() {
+		return this.氏名;
+	}
+
 }
--- src/test/java/jdddd/domain/model/kojin/KojinTest.java
+++ src/test/java/jdddd/domain/model/kojin/KojinTest.java
@@ -33,4 +33,25 @@ public class KojinTest {
 		assertNotEquals(new Kojin.Builder(new AtenaBangou("1234567890"), new KaiseiSeq(1)).build(), kojin);
 		assertNotEquals(new Kojin.Builder(new AtenaBangou("1234567891"), new KaiseiSeq(1)).build(), kojin);
 	}
+
+	@Test
+	public void test世帯番号() {
+		kojin = new Kojin.Builder(new AtenaBangou("1111111111"), new KaiseiSeq(0)).世帯番号(new SetaiBangou("2222222222"))
+				.build();
+		assertEquals(new SetaiBangou("2222222222"), kojin.世帯番号());
+	}
+
+	@Test
+	public void test住民区分() {
+		kojin = new Kojin.Builder(new AtenaBangou("1111111111"), new KaiseiSeq(0)).住民区分(JuuminKubun.日本人).build();
+		assertEquals(JuuminKubun.日本人, kojin.住民区分());
+	}
+
+	@Test
+	public void test氏名() {
+		kojin = new Kojin.Builder(new AtenaBangou("1111111111"), new KaiseiSeq(0))
+				.氏名(new Simei("漢字氏名", "英字氏名", "通称", "0")).build();
+		assertEquals(new Simei("漢字氏名", "英字氏名", "通称", "0"), kojin.氏名());
+	}
+
 }
